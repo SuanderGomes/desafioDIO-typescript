6fb914bf52e5970f3e3ecf0826e28d3c8b06a247
Suander Gomes de Souza <suandergs@hotmail.com>
2022-07-10 19:00:08 -0300

    atualizando os arquivos ts do desafio
    Ainda falta atualizar o ts do desafio 4.

--- src/respostas/desafio2.ts
+++ src/respostas/desafio2.ts
@@ -1,34 +1,34 @@
+// Como podemos melhorar o esse código usando TS?  (Desafio 2)
+interface IPessoa {
+    nome: string;
+    idade: number | string;
+    profissao: Trabalho
+}
 
 enum Trabalho {
     Atriz,
     Padeiro
 }
 
-type Humano = {
-    nome: string,
-    idade: number,
-    profissao: Trabalho
-}
-
-let pessoa1: Humano = {
-    nome: 'maria',
+let pessoa1: IPessoa = {
+    nome: "maria",
     idade: 29,
     profissao: Trabalho.Atriz
 };
 
-let pessoa2: Humano = {
-    nome: 'roberto',
+let pessoa2: IPessoa = {
+    nome: "roberto",
     idade: 19,
     profissao: Trabalho.Padeiro
-};
+}
 
-let pessoa3: Humano = {
-    nome: 'laura',
-    idade: 32,
+let pessoa3: IPessoa = {
+    nome: "laura",
+    idade: "32",
     profissao: Trabalho.Atriz
 };
 
-let pessoa4: Humano = {
+let pessoa4: IPessoa = {
     nome: "carlos",
     idade: 19,
     profissao: Trabalho.Padeiro
